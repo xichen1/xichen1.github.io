1665cc909d075a169634219de4ae33c2676b0bd3
xichen1 <xichen.pan@gmail.com>
2023-07-30 14:52:43 +0800

    style: 💄 Add dark mode to body element

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,8 +7,7 @@ import ThemeContextWrapper from "@src/components/ThemeContextWrapper";
 const Layout = ({pageTitle, children}: { pageTitle: string, children: React.ReactNode }) => {
     return (
         <ThemeContextWrapper>
-            <div className={`bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full 
-            selection:bg-gray-50`}>
+            <div className={``}>
                 <NavBar/>
                 <main className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
             </div>
